7b970fe15d457acccd20f8121b843fd74c34a746
Penny <radiogaga66@gmail.com>
2019-02-08 16:37:45 -0600

    pushing  stripe.js file to see if we can incorporate this on the donate input page

---
+++ stripe.js
@@ -0,0 +1,11 @@
+<form action="your-server-side-code" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_5rdGccNTzD2w5Y12Ib7JcNTH"
+    data-amount="999"
+    data-name="Demo Site"
+    data-description="Example charge"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto">
+  </script>
+</form>
