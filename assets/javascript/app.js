55660014a7260bc0ec6c9ec03fef1876da731c38
Dina Khouri <dina.khouri.91@gmail.com>
2019-02-10 16:53:25 -0600

    location shows when u login and when u create account i cant beleive this is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -309,21 +309,31 @@ function firebaseCreate(
   $(".email").text("Email:" + email);
   $(".Story").text(story);
   $(".profile-img").attr("src", image);
-  console.log(lat);
-  console.log(long);
+
   $("#MapBtn").on("click", function() {
-    var coords = new google.maps.LatLng(lat, long);
+    x = navigator.geolocation;
 
-    var mapOptions = {
-      zoom: 11,
-      center: coords,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    x.getCurrentPosition(success, failure);
 
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    marker = new google.maps.Marker({ map: map, position: coords });
-    mapar.push(map);
-    markar.push(marker);
+    function success(position) {
+      myLat = position.coords.latitude;
+      myLong = position.coords.longitude;
+
+      var coords = new google.maps.LatLng(myLat, myLong);
+
+      var mapOptions = {
+        zoom: 11,
+        center: coords,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      marker = new google.maps.Marker({ map: map, position: coords });
+      mapar.push(map);
+      markar.push(marker);
+    }
+
+    function failure() {}
   });
 
   var logoutBtn = $("<button>");
@@ -355,17 +365,7 @@ function firebaseLogin(email, password) {
         var image = snapshot.val().image;
         var lat = snapshot.val().lat;
         var long = snapshot.val().long;
-        //it is console loging them correctly at this point , but cant seem to show them in the attr
-        // console.log(lat);
-        // console.log(long);
-        // console.log(
-        //   "src",
-        //   "https://maps.google.com/maps?q=" +
-        //     lat +
-        //     "," +
-        //     long +
-        //     "&hl=es;z=14&amp;output=embed"
-        // );
+
         if (mail == email) {
           $("#userDisplay").empty();
 
@@ -378,6 +378,7 @@ function firebaseLogin(email, password) {
           $(".email").text("Email:" + email);
           $(".Story").text(story);
           $(".profile-img").attr("src", image);
+
           $("#MapBtn").on("click", function() {
             var coords = new google.maps.LatLng(lat, long);
 
