de6385e5cf1056b02dfad76713bfd069d784fce4
Dina Khouri <dina.khouri.91@gmail.com>
2019-02-11 13:30:30 -0600

    okay figured what the problem is, line 136-152, i commented that out and as a result sign in and create account location works, need to figure how to show location for the table results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,26 +34,13 @@ $(".show").on("click", function() {
 
     myLatd = myLat;
     myLongd = myLong;
-
-    // var coords = new google.maps.LatLng(myLat, myLong);
-
-    // var mapOptions = {
-    //   zoom: 11,
-    //   center: coords,
-    //   mapTypeId: google.maps.MapTypeId.ROADMAP
-    //};
-
-    // map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    // marker = new google.maps.Marker({ map: map, position: coords });
-    // mapar.push(map);
-    // markar.push(marker);
   }
 
   function failure() {}
 });
 
 // SEARCH BUTTON FUNCTION
-// event listener for search-button that queries the database for user profiles that have the value of "on" for the selected option (optionText).
+// event listener for search-button that queries the database for user profiles that have the value of "on" for the selected option (optionText)
 $(document).on("click", "#search-button", function(event) {
   console.log("search button clicked!");
   event.preventDefault();
@@ -146,219 +133,23 @@ $(document).on("click", "#popupBtn", function(event) {
   $(".Story").text(story);
   $(".profile-imgpop").attr("src", image);
 
-  $("#MapBtnpopup").on("click", function() {
-    var lat = $(this).attr("lat");
-    var long = $(this).attr("long");
-    var coords = new google.maps.LatLng(lat, long);
+  // $("#MapBtnpopup").on("click", function() {
+  //   lat = $(this).attr("lat");
+  //   long = $(this).attr("long");
+  //   var coords = new google.maps.LatLng(lat, long);
 
-    var mapOptions = {
-      zoom: 11,
-      center: coords,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+  //   var mapOptions = {
+  //     zoom: 11,
+  //     center: coords,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
 
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    marker = new google.maps.Marker({ map: map, position: coords });
-    mapar.push(map);
-    markar.push(marker);
-  });
+  //   map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //   marker = new google.maps.Marker({ map: map, position: coords });
+  //   mapar.push(map);
+  //   markar.push(marker);
+  // });
 });
-// Optional design tool that highlights in red your current mouse target:
-//
-// $('body').mousemove(function(event){
-//   $(".red").removeClass("red");
-//   $(event.target).addClass("red");
-// });
-
-// *********************************
-// *********************************
-// RESULTS TO FULL PROFILE FUNCTION
-// *********************************
-// $(document).on("click", ".resultsclick", function(event) {
-//   console.log("clicked!");
-//   // window.location.replace("UserProfile.html");
-//   // mouse over animation
-//   var key = $(this).attr("id");
-//   console.log(key);
-
-// var userRef = firebase.database().ref("Users");
-//** let Bkey = $('#'+key).val();
-// let okey = $(this).attr('id');
-// database.ref("Users").child(Key).remove();
-// $('#'+key).val()
-
-// need for this to be specific to clicked division, not all divisions
-
-// var key = $(".resultsclick").val().key;
-// var key = $(".resultsclick").text("id");
-// var email = $(".resultsclick").attr("email");
-// var password = $(".resultsclick").attr("password");
-
-// setTimeout(function(){ myWindow.close() }, 3000);
-// console.log(okey);
-// console.log(email);
-// console.log(password);
-
-//   var userRef = firebase.database().ref("Users");
-//   userRef
-//     .orderByKey()
-//     .equalTo(key)
-//     .on("child_added", function(snapshot) {
-//       // var key = snapshot.key;
-//       var data = snapshot.val();
-//       var mail = snapshot.val().email;
-//       var user = snapshot.val().username;
-//       var age = snapshot.val().age;
-//       var phone = snapshot.val().phone;
-//       var story = snapshot.val().story;
-//       var image = snapshot.val().image;
-//       var lat = snapshot.val().lat;
-//       var long = snapshot.val().long;
-
-//       console.log(name);
-
-//       var newWindow;
-//       newWindow = window.open("", "newWindow", "width=600, height=900");
-
-//       var windowInsertVar = "";
-
-//       windowInsertVar += "\x3Cscript>";
-
-//       windowInsertVar += "function resizeText(change) {";
-//       windowInsertVar += "switch (change) {";
-//       windowInsertVar += "case 1:";
-//       // windowInsertVar += 'document.getElementById(\'textToBeResized\').style.fontSize = "80%"\;';
-//       windowInsertVar += "break;";
-//       windowInsertVar += "case 2:";
-//       // windowInsertVar += 'document.getElementById(\'textToBeResized\').style.fontSize = "120%"\;';
-//       windowInsertVar += "break;";
-//       windowInsertVar += "default:";
-//       // windowInsertVar += 'alert(\'Error\')\;';
-//       windowInsertVar += "<title>User Profile</title>";
-//       windowInsertVar += "break;";
-//       windowInsertVar += "}";
-//       windowInsertVar += "}";
-//       windowInsertVar += "\x3C/script>";
-//       windowInsertVar +=
-//         '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>';
-//       // windowInsertVar += '<link rel="stylesheet" type="text/css" href="style.css"></link>'
-//       // windowInsertVar += '<button onclick="resizeText(1)">-</button> Text size <button onclick="resizeText(2)">+</button>)\;';
-//       windowInsertVar +=
-//         '<div id="windowHead "><h1>Austin Giving Connection</h1></div>';
-//       windowInsertVar +=
-//         '<div class="container my-container"><div class="row"><div class="col-sm-4"><img class="profile-img row" height="400px" width="400px" src="' +
-//         image +
-//         '"/><h2 id="name-row">Username: ' +
-//         user +
-//         '</h2><h3 class="age row">Age: ' +
-//         age +
-//         '</h3><h3 class="phone row">Phone#: ' +
-//         phone +
-//         '</h3><h3 class="email row">Email: ' +
-//         mail +
-//         '</h3><h3 id="name-row">My Story: ' +
-//         story +
-//         "</h3></div></div></div>";
-//       windowInsertVar +=
-//         '<button onclick=""><i class="fa fa-map-marker" style="font-size:20px;color:red" aria-hidden="true"></i> Find on Map</button>';
-//       // windowInsertVar += '<button onclick="', getSpot(lat, long) +'">get Spot</button>';
-//       // windowInsertVar += '<div class="container my-container"><div class="row"><div class="col-sm-4"><h2 id="name-row">Needs: '+ needs + '</h2>'
-
-//       // windowInsertVar += '$("#MapBtn").on("click",
-
-//       function getSpot(lat, long) {
-//         console.log("getSpot button clicked!");
-//         var coords = new google.maps.LatLng(lat, long);
-
-//         var mapOptions = {
-//           zoom: 11,
-//           center: coords,
-//           mapTypeId: google.maps.MapTypeId.ROADMAP
-//         };
-
-//         map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//         marker = new google.maps.Marker({ map: map, position: coords });
-//         mapar.push(map);
-//         markar.push(marker);
-//       }
-
-//       newWindow.document.write(windowInsertVar);
-//       newWindow.document.write(
-//         '<html><head><title>User Profile</title><link rel="stylesheet" type="text/css" href="style.css"></head><body>'
-//       );
-//       console.log(key);
-//     });
-// });
-
-// firebasePull(key);
-
-// function firebasePull(key) {
-//   // database.ref("Users").on("child_added", function(snapshot) {
-
-//       var userRef = firebase.database().ref("Users");
-//       userRef.orderByChild(key).equalTo(key).on("child_added", function(snapshot) {
-// var optionText = $("#red-sort option:selected").text();
-// console.log("this user needs " + optionText + ":", snapshot.val().username);
-
-// NEED
-// var mail = snapshot.val().email;
-// var user = snapshot.val().username;
-// var age = snapshot.val().age;
-// var name = snapshot.val().name;
-// var phone = snapshot.val().phone;
-// var story = snapshot.val().story;
-// var image = snapshot.val().image;
-// var lat = snapshot.val().lat;
-// var long = snapshot.val().long;
-
-//   var showUser = $("<p>");
-//   showUser.attr("class", "navbar-text navbar-right");
-//   showUser.text("Signed in as " + user);
-//   $(".Name").text(name);
-//   $(".age").text("Age:" + age);
-//   $(".phone").text("phone#:" + phone);
-//   $(".email").text("Email:" + email);
-//   $(".Story").text(story);
-//   $(".profile-img").attr("src", image);
-
-// NEED
-
-// $("#MapBtn").on("click", function() {
-//   var coords = new google.maps.LatLng(lat, long);
-
-//   var mapOptions = {
-//     zoom: 11,
-//     center: coords,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-
-//   map = new google.maps.Map(
-//     document.getElementById("map"),
-//     mapOptions
-//   );
-//   marker = new google.maps.Marker({ map: map, position: coords });
-//   mapar.push(map);
-//   markar.push(marker);
-// });
-
-// var myWindow = window.open("", "", "width=700, height=1000");
-// myWindow.document.write(key).html();
-// myWindow.document.write($(".resultsclick").html());
-
-// var logoutBtn = $("<button>");
-// logoutBtn.attr("class", "btn btn-default nav-item navbar-right");
-// logoutBtn.attr("id", "logout");
-// logoutBtn.text("Logout");
-
-// $("#userDisplay").append(logoutBtn, showUser);
-
-// console.log("You're logged in!");
-// })
-//       })
-//     };
-// });
-
-// -------------------------------------------------------------------------------------------------------
 
 // user login code starts here
 
@@ -384,10 +175,7 @@ $(document.body).on("click", "#create", function() {
   var image = $("#image-input")
     .val()
     .trim();
-  // console.log(myLatd);
-  // console.log(myLongd);
-  // myLatd;
-  // myLongd;
+
   var shoes = $("input:checkbox[name=shoes]:checked").val() || null;
   var tissues = $("input:checkbox[name=tissues]:checked").val() || null;
   var towels = $("input:checkbox[name=towels]:checked").val() || null;
